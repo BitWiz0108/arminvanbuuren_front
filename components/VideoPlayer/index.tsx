d62666a49163e1a48cd5866cab008d55e8b8bb0e
David-Clayton <david@lionsandlegacy.com>
2023-05-29 04:30:20 -0700

    fix: video loading time shrunk

--- components/VideoPlayer/index.tsx
+++ components/VideoPlayer/index.tsx
@@ -19,7 +19,7 @@ const VideoPlayer = (props: any) => {
     setIsVideoLoading(true);
     setTimeout(() => {
       setIsGlobalLoading(false);
-    }, 3000);
+    }, 2000);
   }, [props.src]);
 
   return (
