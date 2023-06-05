0dd5d88a532ebae1772d9d0cdca06dca52dea04a
David-Clayton <david@lionsandlegacy.com>
2023-06-04 21:16:02 -0700

    fix: sidebar and music card shadow effect

--- components/ButtonSidebar/index.tsx
+++ components/ButtonSidebar/index.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 import { motion, AnimatePresence } from "framer-motion";
 
+import { useSizeValues } from "@/contexts/contextSize";
+
 type Props = {
   active: boolean;
   collapsed: boolean;
@@ -19,6 +21,8 @@ const ButtonSidebar = ({
   onClick,
   lastOne = false,
 }: Props) => {
+  const { isMobile } = useSizeValues();
+
   const [hovered, setHovered] = useState<boolean>(false);
 
   const onHover = () => {
@@ -36,7 +40,9 @@ const ButtonSidebar = ({
       className={twMerge(
         "relative w-full h-[48px] lg:h-[58px] flex flex-row justify-start items-center overflow-hidden cursor-pointer",
         collapsed ? "pl-6" : "pl-6 lg:pl-10",
-        lastOne
+        isMobile
+          ? ""
+          : lastOne
           ? "border-t-[0.1rem] border-b-[0.1rem] border-[#464646]"
           : "border-t-[0.1rem] border-[#464646]"
       )}
@@ -49,13 +55,13 @@ const ButtonSidebar = ({
         {!collapsed && <span className="hidden md:inline-flex">{label}</span>}
       </div>
       {active ? (
-        <div className="absolute -left-[20%] top-0 w-[130%] h-full bg-gradient-to-r from-transparent from-0% via-[#0e1959] via-10% to-[#273ccd] to-100% z-0"></div>
+        <div className="absolute -left-[20%] top-0 w-[130%] h-full bg-gradient-to-r from-transparent from-0% via-[#0e1959] via-10% to-[#273ccd] to-100% shadow-inner shadow-black/60 z-0"></div>
       ) : (
         <AnimatePresence>
           {hovered && (
             <motion.div
               className={twMerge(
-                "absolute left-0 top-0 w-[130%] h-full bg-gradient-to-r from-transparent from-0% via-[#0e1959] via-10% to-[#273ccd] to-100%",
+                "absolute left-0 top-0 w-[130%] h-full bg-gradient-to-r from-transparent from-0% via-[#0e1959] via-10% to-[#273ccd] to-100% shadow-inner shadow-black/60",
                 "z-0"
               )}
               initial={{ x: "120%" }}
--- components/MusicCard/index.tsx
+++ components/MusicCard/index.tsx
@@ -55,35 +55,26 @@ const MusicCard = ({
   };
 
   return (
-    <div
-      className={twJoin(
-        "relative rounded-3xl overflow-hidden cursor-pointer select-none border-4",
-        hovered ? "border-bluePrimary" : "border-transparent"
+    <div className="relative">
+      {active && (
+        <div
+          className="absolute -bottom-12 rounded-[50%] -left-[20px] h-8 card-shadow"
+          style={{
+            width: `${
+              isMobile
+                ? MUSIC_CARD_ACTIVE_WIDTH_MOBILE + 40
+                : MUSIC_CARD_ACTIVE_WIDTH_DESKTOP + 40
+            }px`,
+          }}
+        ></div>
       )}
-      style={{
-        width: `${
-          isMobile
-            ? active
-              ? MUSIC_CARD_ACTIVE_WIDTH_MOBILE
-              : MUSIC_CARD_NORMAL_WIDTH_MOBILE
-            : active
-            ? MUSIC_CARD_ACTIVE_WIDTH_DESKTOP
-            : MUSIC_CARD_NORMAL_WIDTH_DESKTOP
-        }px`,
-      }}
-      onMouseEnter={() => onHover()}
-      onMouseLeave={() => onOut()}
-    >
       <div
-        className="w-full"
-        onMouseDown={(e) => setLastPosX(e.screenX)}
-        onMouseUp={(e) => {
-          if (Math.abs(e.screenX - lastPosX) < 30) {
-            onClick();
-          }
-        }}
+        className={twJoin(
+          "relative rounded-3xl overflow-hidden cursor-pointer select-none border-4 shadow-lg",
+          hovered ? "border-bluePrimary" : "border-transparent"
+        )}
         style={{
-          height: `${
+          width: `${
             isMobile
               ? active
                 ? MUSIC_CARD_ACTIVE_WIDTH_MOBILE
@@ -93,55 +84,78 @@ const MusicCard = ({
               : MUSIC_CARD_NORMAL_WIDTH_DESKTOP
           }px`,
         }}
+        onMouseEnter={() => onHover()}
+        onMouseLeave={() => onOut()}
       >
-        <Image
-          className="w-full h-full object-cover select-none pointer-events-none transition-all duration-300"
-          src={music.coverImage ?? PLACEHOLDER_IMAGE}
-          width={1500}
-          height={1500}
-          alt=""
-          placeholder="blur"
-          blurDataURL={IMAGE_BLUR_DATA_URL}
-          priority
-        />
-      </div>
-      <AnimatePresence>
-        {active && !playing && hovered && (
-          <motion.div
-            className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-          >
-            <ButtonCircle
-              dark={false}
-              size="large"
-              icon={<Play width={34} height={34} />}
-              onClick={() => play()}
-            />
-          </motion.div>
-        )}
-      </AnimatePresence>
-      {playing && soundStatus != "none" && hovered && (
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-          {soundStatus == "playing" ? (
-            <Pause
-              width={80}
-              height={80}
-              className="text-primary hover:text-bluePrimary"
-              onClick={() => togglePlay()}
-            />
-          ) : (
-            <ButtonCircle
-              dark={false}
-              size="large"
-              icon={<Play width={34} height={34} />}
-              onClick={() => togglePlay()}
-            />
-          )}
+        <div
+          className="w-full"
+          onMouseDown={(e) => setLastPosX(e.screenX)}
+          onMouseUp={(e) => {
+            if (Math.abs(e.screenX - lastPosX) < 30) {
+              onClick();
+            }
+          }}
+          style={{
+            height: `${
+              isMobile
+                ? active
+                  ? MUSIC_CARD_ACTIVE_WIDTH_MOBILE
+                  : MUSIC_CARD_NORMAL_WIDTH_MOBILE
+                : active
+                ? MUSIC_CARD_ACTIVE_WIDTH_DESKTOP
+                : MUSIC_CARD_NORMAL_WIDTH_DESKTOP
+            }px`,
+          }}
+        >
+          <Image
+            className="w-full h-full object-cover select-none pointer-events-none transition-all duration-300"
+            src={music.coverImage ?? PLACEHOLDER_IMAGE}
+            width={1500}
+            height={1500}
+            alt=""
+            placeholder="blur"
+            blurDataURL={IMAGE_BLUR_DATA_URL}
+            priority
+          />
         </div>
-      )}
+        <AnimatePresence>
+          {active && !playing && hovered && (
+            <motion.div
+              className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <ButtonCircle
+                dark={false}
+                size="large"
+                icon={<Play width={34} height={34} />}
+                onClick={() => play()}
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
+        {playing && soundStatus != "none" && hovered && (
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            {soundStatus == "playing" ? (
+              <Pause
+                width={80}
+                height={80}
+                className="text-primary hover:text-bluePrimary"
+                onClick={() => togglePlay()}
+              />
+            ) : (
+              <ButtonCircle
+                dark={false}
+                size="large"
+                icon={<Play width={34} height={34} />}
+                onClick={() => togglePlay()}
+              />
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
