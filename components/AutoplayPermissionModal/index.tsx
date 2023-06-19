0c32ffe3d77a52f81e1482675ff84af96ef5aa68
David-Clayton <david@lionsandlegacy.com>
2023-06-18 20:40:22 -0700

    feat: 0618 updates

--- components/AutoplayPermissionModal/index.tsx
+++ components/AutoplayPermissionModal/index.tsx
@@ -1,7 +1,4 @@
-import { AnimatePresence, motion } from "framer-motion";
 import { twMerge } from "tailwind-merge";
-
-import X from "@/components/Icons/X";
 
 import { useSizeValues } from "@/contexts/contextSize";
 
@@ -17,39 +14,17 @@ type Props = {
 const AutoplayPermissionModal = ({ isVisible, setVisible, player }: Props) => {
   const { isMobile } = useSizeValues();
 
-  return (
-    <AnimatePresence>
-      {isVisible && (
-        <motion.div
-          className={twMerge(
-            "fixed left-0 top-0 w-screen h-screen p-5 bg-[#000000aa] flex justify-center items-center z-50",
-            isMobile ? "pb-[180px]" : "pb-28 lg:pb-36"
-          )}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-        >
-          <div className="relative w-full md:w-[420px] bg-background h-fit flex flex-col justify-start items-center p-5 rounded-lg">
-            <div className="absolute top-6 right-5 text-primary cursor-pointer">
-              <X
-                width={24}
-                height={24}
-                onClick={() => {
-                  setVisible(false);
-                  player.play();
-                }}
-              />
-            </div>
-
-            <h3 className="px-5 text-md text-center pt-10">
-              Our website will play music in background.
-            </h3>
-          </div>
-        </motion.div>
+  return isVisible ? (
+    <div
+      className={twMerge(
+        "fixed left-0 top-0 w-screen h-screen p-5 bg-transparent flex justify-center items-center z-50",
+        isMobile ? "pb-[180px]" : "pb-28 lg:pb-36"
       )}
-    </AnimatePresence>
-  );
+      onClick={() => {
+        setVisible(false);
+        player.play();
+      }}
+    ></div>
+  ) : null;
 };
-
 export default AutoplayPermissionModal;
